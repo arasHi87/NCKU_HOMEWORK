a31006d6891a031f0241346da304d1ec31c31de4
arashi87 <arasi27676271@gmail.com>
2020-11-04 23:50:36 +0800

    Lec08

--- Program Design/0與1的奇蹟 - Kiseki of 0 & 1.c
+++ Program Design/0與1的奇蹟 - Kiseki of 0 & 1.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
-#define int long long
+#define maxN 40
 
-int a=0, b=0, c=0, idx=32, x, tmp[40];
+int a[maxN], b[maxN], c[maxN];
 
-signed main() {
-	for (int i=1;i<=32;i++) {
-		scanf("%lld", &x); a|=x; a<<=1;
+int main() {
+	for (int i=1;i<=32;i++) scanf("%d", a+i); 
+	for (int i=1;i<=32;i++) scanf("%d", b+i);
+
+	for (int i=32, tmp=0;i>=1;i--) {
+		c[i]=a[i]^b[i]^tmp;
+		tmp=(((a[i] ^ b[i]) && tmp) || (a[i] && b[i]));
 	}
-
-	for (int i=1;i<=32;i++) {
-		scanf("%lld", &x); b|=x; b<<=1;
-	}
-
-	a>>=1, b>>=1, c=a+b;
-
-	while (c) tmp[idx--]=c&1, c>>=1;
-
+	
 	for (int i=1;i<=32;i++)
-		printf("%lld ", tmp[i]);
+		printf("%d ", c[i]);
 }
+
+/* #include <stdio.h> */
+/* #define int long long */
+/*  */
+/* int a=0, b=0, c=0, idx=32, x, tmp[40]; */
+/*  */
+/* signed main() { */
+/*     for (int i=1;i<=32;i++) { */
+/*         scanf("%lld", &x); a|=x; a<<=1; */
+/*     } */
+/*  */
+/*     for (int i=1;i<=32;i++) { */
+/*         scanf("%lld", &x); b|=x; b<<=1; */
+/*     } */
+/*  */
+/*     a>>=1, b>>=1, c=a+b; */
+/*  */
+/*     while (c) tmp[idx--]=c&1, c>>=1; */
+/*  */
+/*     for (int i=1;i<=32;i++) */
+/*         printf("%lld ", tmp[i]); */
+/* } */
