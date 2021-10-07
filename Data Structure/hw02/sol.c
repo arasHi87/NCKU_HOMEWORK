75216e4a4e212109d12eee1cc007088f490906dc
arashi87 <arasi27676271@gmail.com>
2021-10-07 19:43:14 +0800

    fix: fix print newline

--- Data Structure/hw02/sol.c
+++ Data Structure/hw02/sol.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 
-int n;
+int n, flag = 0;
 
 void hanoi(int n, int a, int b, int c)
 {
     if (n) {
         hanoi(n - 1, a, c, b);
-        printf("%d %d %d\n", n, a, c);
+        if (!flag)
+            printf("%d %d %d", n, a, c), flag = 1;
+        else
+            printf("\n%d %d %d", n, a, c);
         hanoi(n - 1, b, a, c);
     }
 }
