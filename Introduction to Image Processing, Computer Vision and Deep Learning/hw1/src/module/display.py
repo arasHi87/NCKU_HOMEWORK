917eefafdfc44aae4dcb56ab9f75a1c6802d5316
arashi87 <arasi27676271@gmail.com>
2021-11-14 06:38:55 +0800

    refactor: extract show image code to standalone function

--- Introduction to Image Processing, Computer Vision and Deep Learning/hw1/src/module/display.py
+++ Introduction to Image Processing, Computer Vision and Deep Learning/hw1/src/module/display.py
@@ -8,30 +8,27 @@ class DisplayWindow(QMainWindow):
         super(DisplayWindow, self).__init__(parent)
 
         self.widget = QWidget()
-        self.image_frame = QLabel()
         self.layout = QVBoxLayout()
-        self.layout.addWidget(self.image_frame)
         self.widget.setLayout(self.layout)
         self.setCentralWidget(self.widget)
 
-    def load_img(self, cv_img):
-        # convert opencv image
+    def add_img_to_window(self, cv_img):
+        img_frame = QLabel()
+        self.layout.addWidget(img_frame)
         q_image = QImage(
             cv_img.data, cv_img.shape[1], cv_img.shape[0], QImage.Format_RGB888
         ).rgbSwapped()
-        self.image_frame.setPixmap(QPixmap.fromImage(q_image))
+        img_frame.setPixmap(QPixmap.fromImage(q_image))
         self.setGeometry(0, 0, cv_img.shape[1], cv_img.shape[0])
+
+    def load_img(self, cv_img):
+        self.add_img_to_window(cv_img)
         print("Height: ", cv_img.shape[0])
         print("Width: ", cv_img.shape[1])
 
     def color_seperation(self, cv_img):
         for channel_index in range(3):
-            frame = QLabel()
-            self.layout.addWidget(frame)
             channel = np.zeros(shape=cv_img.shape, dtype=np.uint8)
             channel[:, :, channel_index] = cv_img[:, :, channel_index]
-            q_image = QImage(
-                channel.data, channel.shape[1], channel.shape[0], QImage.Format_RGB888
-            ).rgbSwapped()
-            frame.setPixmap(QPixmap.fromImage(q_image))
+            self.add_img_to_window(channel)
         self.setGeometry(0, 0, channel.shape[1], channel.shape[0] * 3)
