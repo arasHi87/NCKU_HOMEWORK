0829628d16dcad35e10b35a115b9356b0d6ebd87
arashi87 <arasi27676271@gmail.com>
2022-01-06 04:04:35 +0800

    add iterative merge sort option, and change quick sort wrost test case to all element equal

--- Data Structure/hw27/measure.py
+++ Data Structure/hw27/measure.py
@@ -1,8 +1,8 @@
 import random
 import time
 from subprocess import PIPE, run
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
 from tqdm import trange
 
 
@@ -11,14 +11,23 @@ class Generator:
         self.amount = amount
 
     def get_test_case(self, type):
-        if type < 3:
-            return self._universal(type)
-        return self._heap()
+        if type == 1:
+            return self._quick()
+        if type == 4:
+            return self._heap()
+        return self._universal(type)
 
     def _universal(self, type):
         test_case = "{}\n".format(type)
         for i in range(self.amount, 0, -1):
             test_case += "{} ".format(i)
+        test_case += "\n"
+        return test_case
+
+    def _quick(self):
+        test_case = "1\n"
+        for _ in range(self.amount):
+            test_case += "{} ".format(1)
         test_case += "\n"
         return test_case
 
@@ -33,14 +42,20 @@ class Generator:
 
 
 if __name__ == "__main__":
-    name = ["Insertion sort", "Quick sort", "Merge sort", "Heap sort"]
-    for type in range(4):
+    name = [
+        "Insertion sort",
+        "Quick sort",
+        "Recursive Merge sort",
+        "Iterative Merge sort",
+        "Heap sort",
+    ]
+    for type in range(5):
         x, y = [], []
         tmp = 0
-        for amount in [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000]:
+        for amount in [1000, 2000, 3000, 4000, 5000, 6000]:
             print("Running {} with amount of {}".format(name[type], amount))
             test_case = Generator(amount).get_test_case(type)
-            for _ in trange(100):
+            for _ in trange(20):
                 st = time.time()
                 run(
                     args="./a.out",
@@ -52,7 +67,7 @@ if __name__ == "__main__":
                 )
                 tmp += time.time() - st
             x.append(amount)
-            y.append(tmp / 100)
+            y.append(tmp / 20)
         plt.plot(x, y, label=name[type])
 
     plt.title("Each sort measure")
